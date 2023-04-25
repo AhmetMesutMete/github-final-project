1bde210c753cb3f14595fa6b3aaf0652262a024a
Ahmet Mesut METE <69396993+AhmetMesutMete@users.noreply.github.com>
2023-04-25 21:08:58 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# https://github.com/AhmetMesutMete/
+# AhmetMesutMete
 # Input:
 # p, principal amount
 # t, time period in years
